d0783cfd4d2edb39aeee7e3ea21b23e71880d356
Matthew Toledo <matthew.toledo@dawsoncollege.qc.ca>
2022-11-15 10:41:04 -0500

    create trainer classes

---
+++ app/src/main/java/ca/dawsoncollege/project_pokemon/PlayerTrainer.kt
@@ -0,0 +1,8 @@
+package ca.dawsoncollege.project_pokemon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayerTrainer: Trainer {
+
+}
---
+++ app/src/main/java/ca/dawsoncollege/project_pokemon/Trainer.kt
@@ -0,0 +1,13 @@
+package ca.dawsoncollege.project_pokemon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Trainer {
+
+    val team: ArrayList<Pokemon>;
+    val name;
+
+
+
+}
