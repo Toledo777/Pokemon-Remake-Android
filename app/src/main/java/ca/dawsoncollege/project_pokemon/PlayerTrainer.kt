f39e0cc3d2d3ee4a489998199b55f5fa5956aeb2
Matthew Toledo <matthew.toledo@dawsoncollege.qc.ca>
2022-11-15 11:17:20 -0500

    add skeleton for trainer methods and code catchPokemon

--- app/src/main/java/ca/dawsoncollege/project_pokemon/PlayerTrainer.kt
+++ app/src/main/java/ca/dawsoncollege/project_pokemon/PlayerTrainer.kt
@@ -3,6 +3,24 @@ package ca.dawsoncollege.project_pokemon
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class PlayerTrainer: Trainer {
+class PlayerTrainer: Trainer() {
 
+    val pokemonCollection: ArrayList<Pokemon>;
+    // pokemon center
+
+    // heal all pokemon
+    fun pokemonCenterHeal() {
+
+    }
+
+    // send pokemon from team to box
+    fun sendToBox() {
+
+    }
+
+    // catch wild pokemon
+    fun catchPokemon()
+
+    // switch pokemon on team with one in collection
+    fun changeTeam()
 }
--- app/src/main/java/ca/dawsoncollege/project_pokemon/Trainer.kt
+++ app/src/main/java/ca/dawsoncollege/project_pokemon/Trainer.kt
@@ -3,11 +3,10 @@ package ca.dawsoncollege.project_pokemon
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Trainer {
+// optinal trainer level, only used for enemy trainers
+class Trainer(val trainerLevel: int = null ) {
 
     val team: ArrayList<Pokemon>;
     val name;
-
-
-
+    // used to generate pokemons for enemy trainer
 }
