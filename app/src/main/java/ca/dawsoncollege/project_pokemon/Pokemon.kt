5ddb8f32867e1b5b0e23801e730154223666b919
Paul Patrick Rabanal <paul7098@hotmail.com>
2022-11-18 11:19:07 -0500

    Add experience property

--- app/src/main/java/ca/dawsoncollege/project_pokemon/Pokemon.kt
+++ app/src/main/java/ca/dawsoncollege/project_pokemon/Pokemon.kt
@@ -1,17 +1,19 @@
 package ca.dawsoncollege.project_pokemon
 
+import kotlin.math.pow
+
 // TO ADD:
 // Moves ArrayList
 // Status
-class Pokemon(
-    val species: String,
-    var name: String,
-    var level: Int,
-    var experience: Double,
-    var baseExp: Double,
-    val types: ArrayList<String>,
-    var hp: Int,
-) {
+class Pokemon(var level: Int, var species: String, var name: String? = null) {
+    var experience: Double = 0.0
+
+    init {
+        this.species = this.species.lowercase()
+        this.name = if (this.name == null) this.species else this.name!!.lowercase()
+        this.experience = this.level.toDouble().pow(3.0)
+    }
+
     fun getBattleStats(): BattleStats? {
         return null
     }
