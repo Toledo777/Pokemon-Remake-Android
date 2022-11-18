49d3e4b6e4601e25546b431e7f4b98a253be9847
Paul Patrick Rabanal <paul7098@hotmail.com>
2022-11-18 08:13:05 -0500

    Add method to read JSON data from assets

--- app/src/main/java/ca/dawsoncollege/project_pokemon/JSON.kt
+++ app/src/main/java/ca/dawsoncollege/project_pokemon/JSON.kt
@@ -2,10 +2,19 @@ package ca.dawsoncollege.project_pokemon
 
 import android.content.Context
 import java.io.IOException
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class JSON {
     companion object Helper {
-        fun getAssetJsonData(context: Context, fileName: String): String? {
+        private val gson = Gson()
+
+        fun getJsonData(context: Context, fileName: String): Any {
+            val jsonString = getAssetJson(context, fileName)
+            return gson.toJson(jsonString)
+        }
+
+        private fun getAssetJson(context: Context, fileName: String): String? {
             val jsonString: String
             try {
                 jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
