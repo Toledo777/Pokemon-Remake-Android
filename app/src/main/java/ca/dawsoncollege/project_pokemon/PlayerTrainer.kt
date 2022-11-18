5319237b8dda336be55c7d33601211efee8abc44
Matthew Toledo <matthew.toledo@dawsoncollege.qc.ca>
2022-11-17 21:30:46 -0500

    complete change team method

--- app/src/main/java/ca/dawsoncollege/project_pokemon/PlayerTrainer.kt
+++ app/src/main/java/ca/dawsoncollege/project_pokemon/PlayerTrainer.kt
@@ -2,7 +2,7 @@ package ca.dawsoncollege.project_pokemon
 
 class PlayerTrainer(private val playerName: String): Trainer(null, playerName) {
 
-    private val pokemonCollection: ArrayList<Pokemon> = ArrayList();
+    private val pokemonCollection: ArrayList<Pokemon> = ArrayList()
     // pokemon center
 
     // heal all pokemon
@@ -21,10 +21,17 @@ class PlayerTrainer(private val playerName: String): Trainer(null, playerName) {
     }
 
     // switch pokemon on team with one in collection
+    // NOTE: The new pokemon is gotten using index for now, it can be changed later
+    // to get a pokemon object if more convenient
     fun changeTeam(oldPokemon: Pokemon, newPokemonIndex: Int) {
         // send pokemon from team to box
-        this.team.remove(oldPokemon);
-        pokemonCollection.add(oldPokemon);
+        this.team.remove(oldPokemon)
+        // add team pokemon to box
+        pokemonCollection.add(oldPokemon)
 
+        // remove from box
+        val newPokemon = pokemonCollection.removeAt(newPokemonIndex)
+        // add to team
+        this.team.add(newPokemon)
     }
 }
--- app/src/main/java/ca/dawsoncollege/project_pokemon/Trainer.kt
+++ app/src/main/java/ca/dawsoncollege/project_pokemon/Trainer.kt
@@ -3,8 +3,8 @@ package ca.dawsoncollege.project_pokemon
 // optional trainer level, only used for enemy trainers
 open class Trainer(val trainerLevel: Int? = null, val name: String) {
     //
-    private val MAX_TEAM_CAPACITY = 6;
+    private val MAX_TEAM_CAPACITY = 6
     // set team capacity to 6
-    protected val team: ArrayList<Pokemon> = ArrayList(MAX_TEAM_CAPACITY);
+    protected val team: ArrayList<Pokemon> = ArrayList(MAX_TEAM_CAPACITY)
     // used to generate pokemons for enemy trainer
 }
