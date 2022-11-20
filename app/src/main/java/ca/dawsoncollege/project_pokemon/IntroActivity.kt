f6e42a76dfe7a63d3065cc892e9b6f4e1a7c31c9
unknown <jeremy.tang@dawsoncollege.qc.ca>
2022-11-20 16:33:14 -0500

    add basic validation for trainer name

--- app/src/main/java/ca/dawsoncollege/project_pokemon/IntroActivity.kt
+++ app/src/main/java/ca/dawsoncollege/project_pokemon/IntroActivity.kt
@@ -2,16 +2,32 @@ package ca.dawsoncollege.project_pokemon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ca.dawsoncollege.project_pokemon.databinding.IntroSequenceBinding
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class IntroActivity : AppCompatActivity() {
     private lateinit var binding: IntroSequenceBinding
+    private lateinit var playerTrainer: PlayerTrainer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = IntroSequenceBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.startBtn.setOnClickListener {
+            verifyInputs()
+        }
+    }
 
+    // verifies and validates inputs
+    // TODO: add data validation (no whitespace/special characters in name)
+    private fun verifyInputs(){
+        if (binding.trainerNameInput.text.toString().isBlank()){
+            Toast.makeText(applicationContext, R.string.missing_trainer_name, Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, binding.trainerNameInput.text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
