b91bde103bc1458dac6fb29e0bc3b998031d2700
unknown <jeremy.tang@dawsoncollege.qc.ca>
2022-11-21 15:00:16 -0500

    add selection validation, WIP

--- app/src/main/java/ca/dawsoncollege/project_pokemon/IntroActivity.kt
+++ app/src/main/java/ca/dawsoncollege/project_pokemon/IntroActivity.kt
@@ -11,6 +11,8 @@ import com.google.gson.reflect.TypeToken
 class IntroActivity : AppCompatActivity() {
     private lateinit var binding: IntroSequenceBinding
     private lateinit var playerTrainer: PlayerTrainer
+    private val starters: Map<String, String> = mapOf("grassStarter" to "Bulbasaur",
+        "fireStarter" to "Charmander", "waterStarter" to "Squirtle")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,22 +25,46 @@ class IntroActivity : AppCompatActivity() {
     }
 
     // verifies and validates inputs to create player
-    // TODO: add data validation (no whitespace/special characters in name)
+    // TODO: divide into smaller methods if possible
     private fun verifyInputs(){
+        var starterPokemon = ""
         if (binding.trainerNameInput.text.toString().isBlank()){
             Toast.makeText(applicationContext, R.string.missing_trainer_name, Toast.LENGTH_SHORT).show()
         } else {
-            // TODO: validate starter pokemon nickname
             playerTrainer = PlayerTrainer(binding.trainerNameInput.text.toString())
-            val sharedPreference = getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
-            val editor = sharedPreference.edit()
-            editor.putString("playerTrainer", convertToJSON(playerTrainer))
-            editor.apply()
-//            val json = sharedPreference.getString("playerTrainer", "")
-//            if (json != ""){
-//                playerTrainer = convertToPlayerTrainer(json!!)
-//                Toast.makeText(applicationContext, "Hi " + playerTrainer.name, Toast.LENGTH_SHORT).show()
-//            }
+            if (binding.starterRadioGroup.checkedRadioButtonId == -1){
+                Toast.makeText(applicationContext, R.string.missing_starter_pokemon, Toast.LENGTH_SHORT).show()
+            } else {
+                when (binding.starterRadioGroup.checkedRadioButtonId) {
+                    R.id.grass_starter_img -> {
+                        starterPokemon = starters["grassStarter"].toString()
+                    }
+                    R.id.fire_starter_img -> {
+                        starterPokemon = starters["fireStarter"].toString()
+                    }
+                    R.id.water_starter_img -> {
+                        starterPokemon = starters["waterStarter"].toString()
+                    }
+                }
+                Toast.makeText(applicationContext, "$starterPokemon is your starter!", Toast.LENGTH_SHORT).show()
+
+                if(binding.askNickname.text.toString().isBlank()){
+                    Toast.makeText(applicationContext, "no nickname", Toast.LENGTH_SHORT).show()
+//                    playerTrainer.createStarter(starterPokemon, null)
+                } else {
+                    Toast.makeText(applicationContext, binding.askNickname.text.toString(), Toast.LENGTH_SHORT).show()
+//                    playerTrainer.createStarter(starterPokemon, binding.askNickname.text.toString())
+                }
+                val sharedPreference = getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+                val editor = sharedPreference.edit()
+                editor.putString("playerTrainer", convertToJSON(playerTrainer))
+                editor.apply()
+//                val json = sharedPreference.getString("playerTrainer", "")
+//                if (json != ""){
+//                    playerTrainer = convertToPlayerTrainer(json!!)
+//                    Toast.makeText(applicationContext, "Hi " + playerTrainer.name, Toast.LENGTH_SHORT).show()
+//                }
+            }
         }
     }
 }
