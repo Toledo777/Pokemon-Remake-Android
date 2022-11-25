1acc748e26ecf6c11e5c0b7135d27c0010e61e9a
Paul Patrick Rabanal <paul7098@hotmail.com>
2022-11-25 13:13:27 -0500

    Add method to fetch pokemon moves

--- app/src/main/java/ca/dawsoncollege/project_pokemon/PokeApi.kt
+++ app/src/main/java/ca/dawsoncollege/project_pokemon/PokeApi.kt
@@ -8,4 +8,7 @@ interface PokeApi {
 
     @GET("/api/v2/pokemon/{name}")
     suspend fun getPokemon(@Path("name") name: String): Response<ApiPokemonData>
+
+    @GET("/api/v2/move/{name}")
+    suspend fun getMove(@Path("name") name: String): Response<ApiMoveDetails>
 }
