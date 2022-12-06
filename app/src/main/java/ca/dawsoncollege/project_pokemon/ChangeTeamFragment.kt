9a22c4d1ee5eb99cd68c0daf1ee7ce041b3743de
Paul Patrick Rabanal <paul7098@hotmail.com>
2022-12-06 13:41:37 -0500

    Add fragment for change team

---
+++ app/src/main/java/ca/dawsoncollege/project_pokemon/ChangeTeamFragment.kt
@@ -0,0 +1,15 @@
+package ca.dawsoncollege.project_pokemon
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ChangeTeamFragment : Fragment(R.layout.fragment_change_team) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+    }
