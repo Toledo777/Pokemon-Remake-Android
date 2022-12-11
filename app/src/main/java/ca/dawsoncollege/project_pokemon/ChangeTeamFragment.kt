7a86b4ea3005a5e799ca0783309c72ac338e31e3
Paul Patrick Rabanal <paul7098@hotmail.com>
2022-12-10 19:10:39 -0500

    Fix bug when recyclerview is empty

--- app/src/main/java/ca/dawsoncollege/project_pokemon/ChangeTeamFragment.kt
+++ app/src/main/java/ca/dawsoncollege/project_pokemon/ChangeTeamFragment.kt
@@ -9,41 +9,66 @@ import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
 import ca.dawsoncollege.project_pokemon.databinding.FragmentChangeTeamBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 class ChangeTeamFragment : Fragment(R.layout.fragment_change_team), CustomListener {
 
     private var _binding: FragmentChangeTeamBinding? = null
     private val binding get() = _binding!!
+    private lateinit var userDao: UserDao
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val db = Room.databaseBuilder(
+            requireContext(),
+            AppDatabase::class.java, "Trainer-Database"
+        ).build()
+
+        this.userDao = db.userDao()
+
         _binding = FragmentChangeTeamBinding.inflate(inflater, container, false)
         runBlocking {
+            withContext(Dispatchers.IO) {
+                this@ChangeTeamFragment.userDao.savePlayerTrainer(PlayerTrainer("Paul"))
+            }
             launch(Dispatchers.IO) {
-                _binding!!.recyclerView1.init(
-                    listOf(
+
+                // ADD TEST DATA (TO BE REMOVED)
+                this@ChangeTeamFragment.userDao.updateTeam(
+                    arrayListOf(
                         Pokemon(5, "charmander"),
                         Pokemon(5, "pikachu"),
                         Pokemon(5, "rapidash"),
                         Pokemon(5, "zapdos")
-                    ),
+                    )
+                )
+                _binding!!.recyclerView1.init(
+                    this@ChangeTeamFragment.userDao.fetchPlayerSave().team,
                     binding.emptyListTextView1,
                     LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
                 )
             }
             launch(Dispatchers.IO) {
+                // ADD TEST DATA (TO BE REMOVED)
+                this@ChangeTeamFragment.userDao.updateCollection(
+                    arrayListOf(
+
+                    )
+                )
+
                 _binding!!.recyclerView2.init(
-                    listOf(
-                        Pokemon(5, "squirtle"),
-                        Pokemon(5, "dragonite")
-                    ), binding.emptyListTextView2, GridLayoutManager(context, 6)
+                    this@ChangeTeamFragment.userDao.fetchPlayerSave().pokemonCollection,
+                    binding.emptyListTextView2,
+                    GridLayoutManager(context, 6)
                 )
             }
         }
@@ -65,6 +90,9 @@ class ChangeTeamFragment : Fragment(R.layout.fragment_change_team), CustomListen
         this.adapter = adapter
         emptyTextView.setOnDragListener(adapter.dragInstance)
         this.setOnDragListener(adapter.dragInstance)
+        if (list.isEmpty()) {
+            emptyTextView.visibility = View.VISIBLE;
+        }
     }
 
     override fun setEmptyList(visibility: Int, recyclerView: Int, emptyTextView: Int) {
